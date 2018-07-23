08fd0ecd401d7d0e5beb2a2bc4ad35a2312866e9
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-24 00:15:41 +0200

    Update with score state

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -12,6 +12,8 @@ function PlayState:init()
     self.pipePairs = {}
     self.timer = 0
 
+    self.score = 0 
+
     -- initialize our last recorded Y value for a gap placement to base other gaps off of
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
 end
@@ -38,6 +40,13 @@ function PlayState:update(dt)
 
     -- for every pair of pipes..
     for k, pair in pairs(self.pipePairs) do
+        if not pair.scored then
+            if pair.x + PIPE_WIDTH < self.bird.x then
+                self.score = self.score + 1
+                pair.scored = true
+            end
+        end
+
         -- update position of pair
         pair:update(dt)
     end
@@ -59,14 +68,18 @@ function PlayState:update(dt)
     for k, pair in pairs(self.pipePairs) do
         for l, pipe in pairs(pair.pipes) do
             if self.bird:collides(pipe) then
-                gStateMachine:change('title')
+                gStateMachine:change('score', {
+                    score = self.score
+                })
             end
         end
     end
 
     -- reset if we get to the ground
     if self.bird.y > VIRTUAL_HEIGHT - 15 then
-        gStateMachine:change('title')
+        gStateMachine:change('score', {
+            score = self.score
+        })
     end
 end
 
@@ -74,6 +87,10 @@ function PlayState:render()
     for k, pair in pairs(self.pipePairs) do
         pair:render()
     end
+
+    love.graphics.setFont(fonts['flappyFont'])
+    love.graphics.print('Score: ' .. tostring(self.score), 8, 8)
+
 
     self.bird:render()
 end
